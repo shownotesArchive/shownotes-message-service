33670dc182aaf34a4c91e38db73cc5c6be150bea
Dr4k3 <dr4k3@gmx.net>
2013-09-19 12:16:49 +0200

    new contributer

--- SMS.pl
+++ SMS.pl
@@ -183,7 +183,7 @@ sub showpadinfo {
 # about
 sub about {
     $msg =
-"Shownotes Message Service\n=========================\n\nSource: https://github.com/shownotes/shownotes-message-service\n\nAuthor: Martin Stoffers\nEmail: Dr4k3\@shownot.es\nJabber: Dr4k3\@fastreboot.de\n\nContributers:\n\n Simon Waldherr\n Email:  Simon\@shownot.es\n Jabber: SimonWaldherr\@jabber.shownot.es\n\n\nThis program is published under the terms of GPLv2 and comes with no warranty.\nhttp://www.gnu.org/licenses/gpl-2.0.txt";
+"Shownotes Message Service\n=========================\n\nSource: https://github.com/shownotes/shownotes-message-service\n\nAuthor: Martin Stoffers\nEmail: Dr4k3\@shownot.es\nJabber: Dr4k3\@fastreboot.de\n\nContributers:\n\n Simon Waldherr\n Email:  Simon\@shownot.es\n Jabber: SimonWaldherr\@jabber.shownot.es\n\n Ingolf Gürges\n Jabber: Ingolf.Guerges\@jabber.hftl-brainreset.de\n\n\nThis program is published under the terms of GPLv2 and comes with no warranty.\nhttp://www.gnu.org/licenses/gpl-2.0.txt";
 }
 
 # help
